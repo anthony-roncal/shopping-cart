aff6c6195d0280bd390bb0630fbca2fd7ec2ef92
Anthony <acr1191@gmail.com>
2023-05-24 12:50:46 -0700

    add addition element checks for nav component test

--- src/test/Nav.test.js
+++ src/test/Nav.test.js
@@ -1,10 +1,16 @@
 import { render, screen } from '@testing-library/react';
-import Nav from '../components/Nav';
 import { MemoryRouter } from "react-router-dom";
+import '@testing-library/jest-dom';
+import Nav from '../components/Nav';
 
 describe("Nav component", () => {
-    it("renders correct heading", () => {
-      render(<MemoryRouter><Nav /></MemoryRouter>);
-      expect(screen.getByRole("heading", { name: "Logo Coffee" }).textContent).toMatch(/logo coffee/i);
-    });
+  it("renders all elements correctly", () => {
+    const cartQuantity = 10;
+    render(<MemoryRouter><Nav cartQuantity={cartQuantity}/></MemoryRouter>);
+    expect(screen.getByRole("heading", { name: "Logo Coffee" }).textContent).toMatch(/logo coffee/i);
+    expect(screen.getByText("Home").textContent).toMatch(/home/i);
+    expect(screen.getByText("Shop").textContent).toMatch(/shop/i);
+    expect(screen.getByRole('img')).toBeInTheDocument();
+    expect(screen.getByText('10').textContent).toMatch(cartQuantity.toString());
   });
+});
